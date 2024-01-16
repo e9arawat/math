3c62545f3b31b46557dcbd5d318deca703b439a0
akash rawat <akash.rawat@enine.school>
2024-01-16 16:18:47 +0530

    Completed

---
+++ math027/solution.py
@@ -0,0 +1,34 @@
+"""Day-27"""
+
+
+def answer():
+    """function to find product of the coefficients"""
+
+    def is_prime(num):
+        """function to check if number is prime or not"""
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    a, b, max_length = 0, 0, 0
+    for i in range(-1000, 1000):
+        for j in range(-1000, 1001):
+            count, n = 0, 0
+            while True:
+                num = (n * n) + (i * n) + (j)
+                if is_prime(num):
+                    count += 1
+                    n += 1
+                else:
+                    break
+            if max_length < count:
+                max_length = count
+                a, b = i, j
+    return a * b
+
+
+if __name__ == "__main__":
+    print(answer())
