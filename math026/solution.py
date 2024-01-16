487dee6538e48d7258e74fe2849503a0aa713f57
akash rawat <akash.rawat@enine.school>
2024-01-16 10:55:44 +0530

    Completed

--- math026/solution.py
+++ math026/solution.py
@@ -1,9 +1,9 @@
 """Day-26"""
 
 
-def find_reciprocal(num, reciprocal_list):
+def find_reciprocal(num, reciprocal_dict):
     """function to find longest length of decimal part"""
-    remainder, quotient, temp = [], "0.", 10
+    remainder, quotient, temp = [], "", 10
     while temp not in remainder and temp != 0:
         remainder.append(temp)
         while temp < num:
@@ -12,24 +12,31 @@ def find_reciprocal(num, reciprocal_list):
         quotient += str(temp // num)
         temp = temp % num
         temp *= 10
-
-    reciprocal_list.append(quotient[2:])
+    if temp == 0:
+        reciprocal_dict[num] = quotient
+    else:
+        for index, n in enumerate(remainder):
+            if n == temp:
+                reciprocal_dict[num] = quotient[index:]
     return len(quotient) - 2
 
 
 def solver(n, input_list: list = None):
     """function to find reciprocal"""
-    max_length, reciprocal_list = 0, []
+    max_length, reciprocal_dict = 0, {}
+    ans = 2
     for i in range(2, n + 1):
-        max_length = max(max_length, find_reciprocal(i, reciprocal_list))
+        decimal_length = find_reciprocal(i, reciprocal_dict)
+        if max_length < decimal_length:
+            max_length = decimal_length
+            ans = i
 
     if not input_list:
-        return max_length
-    count = 0
-    for x in reciprocal_list:
-        if str(input_list[0]) in x:
-            count += 1
-    return count
+        return ans
+    for key, value in reciprocal_dict.items():
+        if value == str(input_list[0]):
+            return key
+    return 0
 
 
 def answer():
