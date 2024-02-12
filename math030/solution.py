1db76d270d6985d542f8067d700b1a88c57b4102
e9arawat <153479261+e9arawat@users.noreply.github.com>
2024-02-12 06:44:36 +0530

    Completed

--- math030/solution.py
+++ math030/solution.py
@@ -4,7 +4,7 @@
 def solver(n):
     """function to find sum of all the numbers"""
     ans = 0
-    for i in range(2, 10000000):
+    for i in range(2, 354294):
         temp, index = i, 0
         current_sum = 0
         while temp:
