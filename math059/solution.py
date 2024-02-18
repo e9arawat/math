ba0245d676b629e478811c2f7473b8abcb5fed58
e9arawat <akash.rawat@enine.school>
2024-02-18 19:00:16 +0530

    Completed

--- math059/solution.py
+++ math059/solution.py
@@ -1,5 +1,37 @@
 """
 XOR Decryption
 """
-import string
 
+
+def answer():
+    """
+    XOR Decryption
+    """
+    with open("0059_cipher.txt", "r", encoding="UTF-8") as f:
+        data = [int(num) for num in f.read().split(",")]
+
+    password_length = 3
+    decoded_password = []
+
+    for p in range(password_length):
+        for ch in range(ord("a"), ord("z") + 1):
+
+            dec = []
+            for i in range(p, len(data), password_length):
+                dec.append(chr(data[i] ^ ch))
+
+            count = sum(1 for char in dec if not char.isalpha() and char != " ")
+
+            if count / len(dec) < 0.1:
+                decoded_password.append(ch)
+                break
+
+    decrypted = []
+    for index, x in enumerate(data):
+        decrypted.append(x ^ decoded_password[index % password_length])
+
+    return sum(decrypted)
+
+
+if __name__ == "__main__":
+    print(answer())
